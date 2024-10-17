a47c36b0eba4aa0898bb58ea78a2c15c9e2a900b
Richard Waithe <richardm1@Richards-MacBook-Pro-2.local>
2024-10-17 01:41:37 -0400

    updated create new cards

--- Helix/AddCardButton.swift
+++ Helix/AddCardButton.swift
@@ -9,21 +9,26 @@ import SwiftUI
 
 struct AddCardButton: View {
     var action: () -> Void
+    @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
         Button(action: action) {
-            VStack {
-                Image(systemName: "plus.circle")
-                    .font(.largeTitle)
+            HStack {
+                Image(systemName: "plus")
+                    .font(.title2)
                 Text("Create New Card")
                     .font(.headline)
             }
-            .foregroundColor(AppColors.primary)
+            .foregroundColor(foregroundColor)
         }
         .frame(height: 180)
         .frame(maxWidth: .infinity)
         .background(AppColors.cardGridBackground)
         .cornerRadius(8)
         .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2)
+    }
+    
+    private var foregroundColor: Color {
+        colorScheme == .dark ? Color(hex: 0xdddee3) : Color.black
     }
 }
