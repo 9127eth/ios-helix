ea136ecb4f9c21fe998709c2c9cf23b4ce835da3
9127eth <richard.r.waithe@gmail.com>
2025-12-12 00:24:53 -0500

    updating version number

--- Helix/SettingsView.swift
+++ Helix/SettingsView.swift
@@ -382,7 +382,7 @@ struct SettingsView: View {
                     .padding(.bottom, 20)
 
                     VStack(spacing: 8) {
-                        Text("Version 1.4.5")
+                        Text("Version 1.4.6")
                             .font(.footnote)
                             .foregroundColor(.gray)
                         
